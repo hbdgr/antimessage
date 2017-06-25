1fb7f0ea4756ab6935df37f23398e978501a3079
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-06-25 16:30:40 +0200

    [add] -c option for symmetric encryption
    base64 compression for keys

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate sodiumoxide;
 extern crate rustc_serialize as serialize;
+extern crate rustc_serialize;
 
 extern crate argparse;
 use argparse::{ArgumentParser, StoreTrue, Store};
@@ -10,9 +11,14 @@ use std::io;
 use sodiumoxide::crypto::stream::xsalsa20;
 
 
-struct UserNode {
-    prv_key: u32,
-    pub_key: u32,
+// found here: https://github.com/ustulation/rust_pocs
+fn get_base64_config() -> serialize::base64::Config {
+    rustc_serialize::base64::Config {
+        char_set   : serialize::base64::CharacterSet::Standard,
+        newline    : serialize::base64::Newline::LF,
+        pad        : true,
+        line_length: None,
+    }
 }
 
 fn sodium_secretbox_test() {
@@ -84,6 +90,7 @@ fn print_vec_hexbytes(vector: &Vec<u8>) {
     }
     print!("\n");
 }
+
 fn print_vec_bytes_as_string(vector: &Vec<u8>) {
     for x in vector {
         let ch : char = x.clone() as char;
@@ -98,33 +105,81 @@ fn xsalasa20_get_zero_nonce() -> xsalsa20::Nonce {
     nonce
 }
 
-fn xsalasa20_key_compress(key: &xsalsa20::Key ) {
-    println!("{:?}", key);
+fn xsalasa20_key_compress(key: &xsalsa20::Key) -> String {
+    use serialize::base64::{self,ToBase64,FromBase64};
+
+    let key_b64 = key.0.to_base64(get_base64_config());
+    let key_from_b64 = key_b64.from_base64().ok().unwrap();
+
+    key_b64
 }
 
-fn crypto_stream_encrypt() {
-    use sodiumoxide::crypto::stream::xsalsa20;
+fn xsalasa20_key_decompress(key_b64: &String) -> xsalsa20::Key {
+    use serialize::base64::{self,ToBase64,FromBase64};
 
-    let key = xsalsa20::gen_key();
+    let mut key_from_b64 = key_b64.from_base64().ok().unwrap();
+
+    let xsalsa20_key = xsalsa20::Key::from_slice(key_from_b64.as_mut_slice())
+        .unwrap();
+    xsalsa20_key
+}
+
+fn crypto_stream_encrypt_inter() {
+    use sodiumoxide::crypto::stream::xsalsa20;
 
     let zero_nonce = xsalasa20_get_zero_nonce();
 
     let mut message = read_cmd_message();
-    println!("Key generated for this message (save if, if you want to decrypt your message)");
-    xsalasa20_key_compress(&key);
+    while message.len() == 0 {
+        println!("Empty message, try again: ");
+        message = read_cmd_message();
+    }
 
-    print!("Befor encryption: ");
-    print_vec_bytes_as_string(&message);
+    let key = xsalsa20::gen_key();
+    let key_compressed = xsalasa20_key_compress(&key);
+
+    print!("Key generated for this message ");
+    println!("(keep secure, it is needed to decrypt your message):\n{}", key_compressed);
+
+    let k_decompressed = xsalasa20_key_decompress(&key_compressed);
+    //println!("key decompresedesed {:?}", k_decompressed);
+
+    //print!("Befor encryption: ");
+    //print_vec_bytes_as_string(&message);
+
     // encrypt
     xsalsa20::stream_xor_inplace(message.as_mut_slice(), &zero_nonce, &key);
     println!("Ciphertext");
     print_vec_hexbytes(&message);
 
     // decrypt
+    //xsalsa20::stream_xor_inplace(message.as_mut_slice(), &zero_nonce, &key);
+    //print!("After decryption: ");
+    //print_vec_bytes_as_string(&message);
+}
+
+fn crypto_stream_encrypt(mut message: Vec<u8>) {
+    use sodiumoxide::crypto::stream::xsalsa20;
+
+    let zero_nonce = xsalasa20_get_zero_nonce();
+
+    while message.len() == 0 {
+        println!("Empty message, try again: ");
+        message = read_cmd_message();
+    }
+
+    let key = xsalsa20::gen_key();
+    let key_compressed = xsalasa20_key_compress(&key);
+
+    print!("Key generated for this message ");
+    println!("(keep secure, it is needed to decrypt your message):\n{}", key_compressed);
+
+    let k_decompressed = xsalasa20_key_decompress(&key_compressed);
+
+    // encrypt
     xsalsa20::stream_xor_inplace(message.as_mut_slice(), &zero_nonce, &key);
-    //assert_eq!(plaintext, &mut [0, 1, 2, 3]);
-    print!("After decryption: ");
-    print_vec_bytes_as_string(&message);
+    println!("Ciphertext");
+    print_vec_hexbytes(&message);
 }
 
 fn main() {
@@ -135,28 +190,35 @@ fn main() {
     print_lines_of_clouds(3);
 
     let mut interactive = false;
-    let mut symmetric : String = String::from("");
+    let mut symmetric_msg : String = String::from("");
+    let mut key : String = String::from("");
     {  // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
         ap.set_description("Simple message symmetric encryption, with random key using xsalsa20");
         ap.refer(&mut interactive)
             .add_option(&["-i", "--interactive"], StoreTrue,
             "interactive mode (step by step) for message encryption");
-        ap.refer(&mut symmetric)
+        ap.refer(&mut symmetric_msg)
             .add_option(&["-c", "--symmetric"], Store,
             "encrypt message with random key. Gives key and cipher text as output");
+        ap.refer(&mut key)
+            .add_option(&["-k", "--key"], Store,
+            "key is needed to decrypt message, you have to provide it if you using (-d, --decrypt) options");
         ap.parse_args_or_exit();
     }
 
     if interactive {
-        crypto_stream_encrypt();
+        crypto_stream_encrypt_inter();
         return ();
     }
 
-    if symmetric.len() != 0 {
-        println!("Msg to encrypt: {}", symmetric);
+    if symmetric_msg.len() != 0 {
+        println!("Msg to encrypt: {}", symmetric_msg);
+        let msg_vec = symmetric_msg.into_bytes();
+
+        crypto_stream_encrypt(msg_vec);
     } else {
-        println!("Goodbye");
+        println!("Not to do. Goodbye");
         print_lines_of_clouds(3);
     }
 }
